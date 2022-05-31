a04ceeae28b8a8ea850c46791d419781373d59bb
Lizzy2307 <caceresyani942@gmail.com>
2022-05-30 22:35:01 -0600

    Se agrega el Proyecto

---
+++ src/App.java
@@ -0,0 +1,17 @@
+// Ejercicio 2.27
+//Escriba una aplicación que muestre un patrón de tablero de damas
+
+public class Ejercicio2_27
+{
+    public static void main(String[]args)
+    {
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+    }
+}
